f89f3dbe0ba41599cba278f158cc311117bf1571
George Roberts <georgethomasroberts@gmail.com>
2022-07-29 20:34:10 +0100

    Fix percentage stat

--- src/components/resultCard.jsx
+++ src/components/resultCard.jsx
@@ -2,7 +2,7 @@ import { Text, Divider, Heading } from '@chakra-ui/react'
 import WinTable from './winTable';
 
 const asPercentageOfStartBond = (newValue, startValue) => {
-    return (newValue / startValue * 100 / 100).toFixed(3)
+    return (newValue / startValue * 100 - 100).toFixed(1)
 }
 
 const average = arr => (arr.reduce( ( a, b ) => a + b, 0 ) / arr.length).toFixed(2);
