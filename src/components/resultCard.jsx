b3111c9cd1c38a3dbf913aa7a76b1bb24de1aa62
George Roberts <georgethomasroberts@gmail.com>
2022-07-29 21:17:13 +0100

    Styled updates

--- src/components/resultCard.jsx
+++ src/components/resultCard.jsx
@@ -1,4 +1,7 @@
-import { Text, Divider, Heading } from '@chakra-ui/react'
+import { Box,
+        Text, 
+        Divider, 
+        Heading } from '@chakra-ui/react'
 import WinTable from './winTable';
 
 const asPercentageOfStartBond = (newValue, startValue) => {
@@ -11,15 +14,15 @@ const ResultCard = (props) => {
     const finalValue = props.startBondValue + props.winData.winValue
 
     return (
-        <div>
+        <Box w='100%' p={4} color='black' borderWidth='0.01em' borderRadius='lg' marginBottom='5px'> 
             <Heading as='h3' size='lg'>{props.title}</Heading>
             <Text>Predicted bond value after {props.drawCount} draws: <b>£ {finalValue}</b></Text>
             <Text>Increase as percentage: <b>{asPercentageOfStartBond(finalValue, props.startBondValue)} %</b></Text>
+            <Divider />
             <WinTable winCategories={props.winData.winCategories} />
 
-            <Divider />
             {/* <h3>Average annual value change after {props.yearsInvesting} year(s): £ {props.averageAnnualGain}</h3> */}
-        </div>
+        </Box>
     )
 }
 
